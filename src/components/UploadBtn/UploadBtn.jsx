32e1a38449515206b4e4beff04bafa1ea0fa980f
astha vohra <vohraastha.2712@gmail.com>
2022-02-26 14:11:05 +0000

    fixed padding for header on desktop

--- src/components/UploadBtn/UploadBtn.jsx
+++ src/components/UploadBtn/UploadBtn.jsx
@@ -4,10 +4,12 @@ import Avatar from "../Avatar/Avatar";
 
 function UploadBtn() {
   return (
-    <button className="upload-btn" type="submit">
-      <img className="upload-btn__icon" src={uploadIcon} alt="Upload Icon" />
-      Upload
-    </button>
+    <div className="upload">
+      <button className="upload-btn" type="submit">
+        <img className="upload-btn__icon" src={uploadIcon} alt="Upload Icon" />
+        Upload
+      </button>
+    </div>
   );
 }
 
