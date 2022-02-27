4db6bdc3d9e246c5cacf7ea768a740b2b902f4b8
astha vohra <vohraastha.2712@gmail.com>
2022-02-27 14:28:32 +0000

    cleared extra code and added href for brainflix logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,15 @@ import logo from "../../assets/logo/BrainFlix-logo.svg";
 import SearchBar from "../SearchBar/SearchBar";
 import UploadBtn from "../UploadBtn/UploadBtn";
 import Avatar from "../Avatar/Avatar";
-import CommentForm from "../CommentForm/CommentForm";
-import MediaList from "../MediaList/MediaList";
-import videos from "../../data/videos.json";
 function Header() {
   return (
     <header className="header">
-      <section className="header__logo" href="">
-        <div className="header__logo-div">
-          <img className="header__logo-img" src={logo} alt="BrainFlix Logo" />
-        </div>
+      <section className="header__logo">
+        <a href="/">
+          <div className="header__logo-div">
+            <img className="header__logo-img" src={logo} alt="BrainFlix Logo" />
+          </div>
+        </a>
       </section>
       <section className="header__content">
         <SearchBar />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,13 @@ import "./Hero.scss";
 function Hero({ selectedEntry }) {
   return (
     <section className="hero">
-      <a href="">
-        <video className="hero__video" controls poster={selectedEntry.image}>
-          <source
-            className="hero__video-source"
-            src={selectedEntry.video}
-            type="video/mp4"
-          />
-        </video>
-      </a>
+      <video className="hero__video" controls poster={selectedEntry.image}>
+        <source
+          className="hero__video-source"
+          src={selectedEntry.video}
+          type="video/mp4"
+        />
+      </video>
     </section>
   );
 }
