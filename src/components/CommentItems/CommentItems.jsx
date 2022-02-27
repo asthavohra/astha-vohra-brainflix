c1c46043632e77b554907cf1b00140b78f9aa551
astha vohra <vohraastha.2712@gmail.com>
2022-02-27 11:31:39 +0000

    added moment for relative timestamp

--- src/components/CommentItems/CommentItems.jsx
+++ src/components/CommentItems/CommentItems.jsx
@@ -1,52 +1,6 @@
 import "./CommentItems.scss";
+import moment from "moment";
 
-// Calculate how long ago was the comment posted
-{
-  /*
-const timeAgo = (timestamp) => {
-  let date = new Date(timestamp);
-  let today = new Date();
-  let seconds = Math.round((today - date) / 1000);
-  let minutes = Math.round(seconds / 60);
-  let hours = Math.round(minutes / 60);
-
-  if (seconds < 10) {
-    return `Now`;
-  } else if (seconds < 60) {
-    return `${seconds} seconds ago`;
-  } else if (seconds < 90) {
-    return `A minute ago`;
-  } else if (minutes < 60) {
-    return `${minutes} minutes ago`;
-  } else if (hours < 2) {
-    return `An hour ago`;
-  } else if (hours < 24) {
-    return `${hours} hours ago`;
-  } else {
-    return timestampToDate(timestamp);
-  }
-};
-
-// Transform timestamp in a date format MM/DD/YYYY
-const timestampToDate = (timestamp) => {
-  let date = new Date(timestamp);
-
-  let dd = date.getDate();
-  let mm = date.getMonth() + 1;
-  let yyyy = date.getFullYear();
-
-  if (dd < 10) {
-    dd = "0" + dd;
-  }
-
-  if (mm < 10) {
-    mm = "0" + mm;
-  }
-
-  return (date = mm + "/" + dd + "/" + yyyy);
-};
-*/
-}
 function CommentItems({ comments }) {
   return (
     <>
@@ -71,7 +25,7 @@ function CommentItems({ comments }) {
                 </div>
                 <div className="comment__right__main__timestamp">
                   <p className="comment__right__main__timestamp__text">
-                    {new Date(comment.timestamp).toLocaleDateString()}
+                    {moment(comment.timestamp).fromNow()}
                   </p>
                 </div>
               </div>
